2734e0894d44692cb23f3a18ea4dd6dfd4b774f6
sino <smk16sound@gmail.com>
2019-05-16 19:43:46 +0900

    編集

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,8 @@
 $(function(){
   function buildHTML(message){
     if ( message.image ) {
+      Image = `<img src="${message.image}">`
+    } 
       var html =
       `<div class="message" data-message-id=${message.id}>
           <div class="upper-message">
@@ -15,32 +17,13 @@ $(function(){
             <p class="lower-message__content">
               ${message.content}
             </p>
-          </div>
-          <asset_path src=${message.image} >
-        </div>`
-      return html;
-    } else {
-      var html =
-      `<div class="message" data-message-id=${message.id}>
-          <div class="upper-message">
-            <div class="upper-message__user-name">
-              ${message.user_name}
-            </div>
-            <div class="upper-message__date">
-              ${message.date}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
+            ${Image}
           </div>
         </div>`
       return html;
-    };
   };
 
-  $(".new_message").on('submit', function(e){
+  $(".new_message").on('submit', function(e) {
   e.preventDefault();
   var formData = new FormData(this);
   var url = $(this).attr('action');

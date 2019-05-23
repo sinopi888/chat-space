61b69bcb212be7e86bd2dd9008d205eff797ad77
sino <smk16sound@gmail.com>
2019-05-23 20:37:36 +0900

    edit production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
+server '52.196.66.130', user: 'ec2-user', roles: %w{app db web}
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -59,4 +60,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '52.196.66.130', user: 'ec2-user', roles: %w{app db web}
+
